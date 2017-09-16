5e6fd8dfdb04591ab4d1889f2f843d1ecffeffe2
Leonard Grazian <grazian.leonard@gmail.com>
2017-09-16 11:45:13 -0400

    basic get and post functions

--- app.js
+++ app.js
@@ -5,8 +5,31 @@ var handlebars = require('express-handlebars');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// hanldebars middleware
+app.engine('handlebars', handlebars({
+  'defaultLayout': 'main'
+}));
+app.set('view engine', 'handlebars');
 
-app.listen(8000, function () {
-  console.log('server started...');
+// static serving middleware
+var staticOptions = {
+  'index': false
+}
+app.use(express.static(path.join(__dirname, 'public'), staticOptions));
+
+// pick the port
+app.set('port', (process.env.PORT || 8000));
+
+// oh fuck we can use routing now
+app.get('/', function (req, res) {
+  res.render('pretty-chart');
+});
+
+app.post('/register', function (req, res) {
+  console.log('post request recieved...');
+  res.render('dashboard');
+})
+
+app.listen(app.get('port'), function () {
+  console.log('server started on port ' + app.get('port') + '...');
 });
